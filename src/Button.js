6dfbd1735d60ac7659ed60277c0033ba2d097ad9
Kavya Narayan <112636120+nkavya00@users.noreply.github.com>
2023-02-18 13:14:57 -0500

    Update Button.js

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,35 @@
 import React from "react";
-const Button = ({text, onClick}) => {
-    return (
-        <button onClick={onClick}>
-            {text}
-        </button>
 
-    );
-
+const Button = ({text}) => {
+  return (
+    <button
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "18px 36px",
+        gap: "10px",
+        position: "absolute",
+        width: "253px",
+        height: "70px",
+        left: "1px",
+        top: "99px",
+        background: "#0568FD",
+        borderRadius: "3px",
+        fontFamily: "General Sans, sans-serif",
+        fontWeight: "600",
+        fontSize: "16px",
+        color: "#FFFFFF",
+        border: "none",
+        margin: "auto",
+        textAlign: "center"
+      }}
+    //   onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 };
+
 export default Button;
