feb1e0a545bf0c86fcef8662d0ba6ee77bd72cc1
seojuny <123113614+seojuny@users.noreply.github.com>
2023-03-16 13:35:47 -0400

    Add flip and home files

---
+++ src/Flip.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Flip.css'
+
+const Flip = () => {
+  return (
+  <>
+  <div class="flip-card">
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+        <h1>sdfsdsdf</h1>
+        <p>sdjflsdf</p>
+        </div>
+        <div class="flip-card-back">
+          <h1>stuff</h1>
+          <p>sofs</p>
+        </div>
+      </div>
+  </div>
+  </>
+  );
+};
+
+export default Flip;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header.js';
+import Flip from './Flip.js'
+import './Home.css'
+
+const Home = () => {
+    return (
+      <div>
+        <Header></Header>
+        <Flip></Flip>
+      </div>
+    );
+  }
+  
+  export default Home;
